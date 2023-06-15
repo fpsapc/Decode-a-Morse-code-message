bb4b30d3adcb866a86ac4d9bc2f7e75b26fb7249
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-06-15 21:51:46 +0500

    Add decode_morse_code and readme files

---
+++ decode_morse_code.rb
@@ -0,0 +1,28 @@
+def decode_char(morse_code)
+  morse_code_table = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+
+  morse_code_table[morse_code] || ''
+end
+
+def decode_word(morse_word)
+  morse_chars = morse_word.split
+  decoded_word = ''
+
+  morse_chars.each do |morse_char|
+    decoded_char = decode_char(morse_char)
+    decoded_word += decoded_char
+  end
+
+  decoded_word
+end
+
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = decode(message)
+puts decoded_message
